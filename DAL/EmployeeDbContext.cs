922ac03558b1e69cfc955dc6fa8974b0e7b906ce
Aijaz Ahmad <aijazahmad4276@gmail.com>
2024-09-05 20:51:11 +0530

    Add files via upload

---
+++ DAL/EmployeeDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Application_Form_MVC.Models.DBEntities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application_Form_MVC.DAL
+{
+    public class EmployeeDbContext : DbContext
+    {
+        public EmployeeDbContext(DbContextOptions options) : base(options) { }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/EmployeeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Application_Form_MVC.Models
+{
+    public class EmployeeViewModel
+    {
+        public int Id { get; set; }
+
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [DisplayName("Date Of Birth")]
+        public string DateOfBirth { get; set; }
+
+        [DisplayName("E-Mail")]
+        public string Email { get; set; }
+        public string Salary { get; set; }
+
+        [DisplayName("Name")]
+        public string FullName
+        {
+            get
+            {
+                return FirstName + " " + LastName;
+            }
+        }
+    }
+}
